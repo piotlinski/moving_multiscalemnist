08585164bf113abe677a31971434dde90ce92091
Piotr Zieliński <piotrekzie100@gmail.com>
2022-05-19 12:33:50 +0200

    Add CLI

---
+++ moving_multiscalemnist/__main__.py
@@ -0,0 +1,83 @@
+import argparse
+import logging
+
+from moving_multiscalemnist.defaults import (
+    DATA_DIR,
+    FPS,
+    IMAGE_SIZE,
+    MAX_DIGITS,
+    MIN_DIGITS,
+    N_FRAMES,
+    OSCILLATIONS,
+    SIZES,
+    TEST_SIZE,
+    TRAIN_SIZE,
+)
+from moving_multiscalemnist.generate import generate_dataset
+
+logging.basicConfig(level=logging.INFO)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data-dir", "-d", help="MNIST location", default=DATA_DIR)
+    parser.add_argument(
+        "--train-size", "-trs", help="Train subset size", type=int, default=TRAIN_SIZE
+    )
+    parser.add_argument(
+        "--test-size", "-tss", help="Test subset size", type=int, default=TEST_SIZE
+    )
+    parser.add_argument(
+        "--n-frames",
+        "-nf",
+        help="Number of frames in a sequence",
+        type=int,
+        default=N_FRAMES,
+    )
+    parser.add_argument(
+        "--min-digits",
+        help="Minimum number of digits in a sequence",
+        type=int,
+        default=MIN_DIGITS,
+    )
+    parser.add_argument(
+        "--max-digits",
+        help="Maximum number of digits in a sequence",
+        type=int,
+        default=MAX_DIGITS,
+    )
+    parser.add_argument(
+        "--image-size",
+        "-is",
+        help="Output image size",
+        nargs=2,
+        type=int,
+        default=IMAGE_SIZE,
+    )
+    parser.add_argument(
+        "--sizes", "-ss", help="Digit sizes", nargs="+", type=int, default=SIZES
+    )
+    parser.add_argument(
+        "--oscillations",
+        "-o",
+        help="Size oscillation period",
+        nargs="+",
+        type=float,
+        default=OSCILLATIONS,
+    )
+    parser.add_argument(
+        "--fps", help="Frames per second (period length)", type=float, default=FPS
+    )
+
+    args = parser.parse_args()
+    generate_dataset(
+        data_dir=args.data_dir,
+        train_size=args.train_size,
+        test_size=args.test_size,
+        n_frames=args.n_frames,
+        min_digits=args.min_digits,
+        max_digits=args.max_digits,
+        image_size=args.image_size,
+        sizes=args.sizes,
+        oscillations=args.oscillations,
+        fps=args.fps,
+    )
